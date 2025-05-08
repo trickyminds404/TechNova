542c934ddd4aab09779550a7e9e56dc06d4fc7b2
trickyminds404 <nagesh25.aws.acc@gmail.com>
2025-05-08 19:41:32 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => res.send('TechNova Inventory App'));
+app.get('/', (req, res) => res.send('TechNova'));
 app.listen(3000, () => console.log('Server running on port 3000'));
 
