0ed841686e28973c52adbdf6af82722d5a98a86f
Raj <Raj>
2025-05-08 13:27:59 +0000

    Initial commit with sample app

---
+++ app.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => res.send('TechNova Inventory App'));
+app.listen(3000, () => console.log('Server running on port 3000'));
+
